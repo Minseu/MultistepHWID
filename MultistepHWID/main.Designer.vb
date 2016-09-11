e99e02ab02b6313c0fc17b0a997570d06e256b53
Tyler Hill <tylerhill@minseu.kr>
2016-09-11 03:48:10 -0500

    init
    init

---
+++ MultistepHWID/main.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(15, 103)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(206, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Register HWID"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "HWID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "IPv4:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "RM:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(66, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(155, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(66, 26)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(155, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(66, 47)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(155, 22)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 78)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "eHWID:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(66, 75)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(155, 22)
+        Me.TextBox4.TabIndex = 10
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 143)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Multi-Step HWID //DEMO"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ MultistepHWID/main.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Management
+
+Public Class main
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim HWID As String = String.Empty
+        Dim mc1 As New ManagementClass("win32_processor")
+        Dim kirsten As String = "192.158.123.344"
+        Dim MOBC As ManagementObjectCollection = mc1.GetInstances()
+        ' DIM 
+        For Each mob As ManagementObject In MOBC
+            If HWID = "" Then
+                HWID = mob.Properties("processorID").Value.ToString()
+                Exit For
+            End If
+        Next
+        TextBox1.Text = HWID
+        ' END PROCESSORID
+        ' Start IP
+        GetIPV4()
+        TextBox2.Text = GetIPV4()
+        ' END IPV4
+        ' START RM
+        TextBox3.Text = My.Computer.Info.TotalPhysicalMemory
+        ' END RM
+        ' START FINAL HWID
+        Dim RAM As String = TextBox2.Text
+        RAM = RAM.Replace(".", "")
+        Dim eHWID As String = RAM + TextBox1.Text + TextBox3.Text
+        TextBox4.Text = getSHA1Hash(eHWID)
+    End Sub
+    Private Function GetIPV4() As String
+        GetIPV4 = String.Empty
+        Dim strHostName As String = System.Net.Dns.GetHostName()
+        Dim ipheas As System.Net.IPHostEntry = System.Net.Dns.GetHostEntry(strHostName)
+        For Each ipheal As System.Net.IPAddress In ipheas.AddressList
+            If ipheal.AddressFamily = System.Net.Sockets.AddressFamily.InterNetwork Then
+                GetIPV4 = ipheal.ToString
+            End If
+        Next
+    End Function
+    Function getSHA1Hash(ByVal strToHash As String) As String
+
+        Dim sha1Obj As New Security.Cryptography.SHA1CryptoServiceProvider
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+        bytesToHash = sha1Obj.ComputeHash(bytesToHash)
+        Dim strResult As String = ""
+        For Each b As Byte In bytesToHash
+            strResult += b.ToString("x2")
+        Next
+        Return strResult
+    End Function
+End Class
